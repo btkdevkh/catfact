d3277e829de771b119176aa6e5894f24bc134583
btkdevkhdelli5 <amcjkbtk91@outlook.com>
2022-05-10 14:35:17 +0200

    Make some change

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="text-center text-white">
     <h2 class="text-3xl mb-5">VueJS Web App | Cat Ninja 1.0.0</h2>
-    <p class="text-lg">API by Mr Julien Petit</p>
   </div>
 </template>
